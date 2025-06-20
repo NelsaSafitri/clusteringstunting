462eb3c74b41800d8d82c1dc63cd3be09610599a
Nelsa Safitri <146648667+NelsaSafitri@users.noreply.github.com>
2025-06-20 18:51:16 +0700

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.title("Clustering Stunting Balita dengan K-Means")
+
 @st.cache_data
 def load_data():
     try:
